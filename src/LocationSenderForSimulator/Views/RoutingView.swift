c52922ccdca71e19c39fb0e9dfacb72e19468dc7
Marc-Alexandre Blanchard <marc-alexandre.blanchard@smart-origin.com>
2025-10-28 11:28:30 +0100

    [new feature] implements GeoJSON loading

--- src/LocationSenderForSimulator/Views/RoutingView.swift
+++ src/LocationSenderForSimulator/Views/RoutingView.swift
@@ -1,8 +1,92 @@
 import SwiftUI
 
 struct RoutingView: View {
+    /// to display file importer
+    @State private var fileImporterPresented:Bool = false
+    
+    /// route loaded by user
+    @State private var route:Route? = nil
+    
+    /// to display aler
+    @State private var hasError = false
+    
+    /// error presented in alert if any
+    @State private var error:Error? = nil
+    
     var body: some View {
-        Text("hello")
+        VStack{
+            HStack {
+                Button("button.pickRoute.text", systemImage: "folder") {
+                    self.fileImporterPresented = true
+                }
+                Spacer()
+                Button {
+                    if let url = URL(string:  "https://geojson.org"){
+                        EnvironmentValues().openURL(url)
+                    }
+                } label: {
+                    Image(systemName: "questionmark.text.page")
+                }
+            }
+            if(hasRoute()) {
+                GroupBox {
+                    HStack {
+                        HStack {
+                            Text("text.geosjson.text")
+                            Text(self.route!.filePath).bold()
+                        }
+                        Spacer()
+                        Button {
+                            self.clearGeoJson()
+                        } label: {
+                            Image(systemName: "xmark")
+                        } .buttonStyle(.borderless)
+                    }
+                }
+            }
+            Spacer()
+        }.fileImporter(isPresented: self.$fileImporterPresented, allowedContentTypes:[.geoJSON, .json, .text, .plainText], onCompletion: { (res) in
+            switch res {
+            case .success(_):
+                do {
+                    self.route = try decode(geoJsonFile: try res.get())
+                }
+                catch{
+                    self.declareError(err: error)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        })
+        .alert(isPresented: self.$hasError) {
+            if let err = error{
+                Alert(title: Text("alert.error.title"),
+                      message: Text(err.localizedDescription),
+                      dismissButton: .default(Text("alter.error.okButton")))
+            }
+            else {
+                Alert(title: Text("alert.error.title"),
+                      message: Text("alert.error.unknownMessage"),
+                      dismissButton: .default(Text("alter.error.okButton")))
+            }
+        }
+        .padding(5)
+    }
+    
+    /// to display an error in alert
+    private func declareError(err:Error){
+        self.hasError = true
+        self.error = err
+    }
+    
+    /// true if valid geojson has been loaded
+    private func hasRoute() -> Bool {
+        return self.route != nil
+    }
+    
+    /// unload current GeoJson
+    private func clearGeoJson() {
+        self.route = nil
     }
 }
 
