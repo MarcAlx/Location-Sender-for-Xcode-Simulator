edd58055d2efe3522758ba5341d1de4af9427248
Marc-Alx <marc-alx@outlook.com>
2025-10-28 16:09:40 +0100

    [improve] default value for playback rate

--- src/LocationSenderForSimulator/AppConfig.swift
+++ src/LocationSenderForSimulator/AppConfig.swift
@@ -8,3 +8,6 @@ public let MAX_POINT_RATE:Int = 5000
 
 /// step for slider of point rate in ms
 public let POINT_RATE_STEP:Int = 100
+
+/// default point rate in ms
+public let DEFAULT_POINT_RATE:Int = 500
